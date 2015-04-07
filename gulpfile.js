ac196836ba3ead57db7c81be30bde4882ee760f8
Beau Hankins <beau.j.hankins@gmail.com>
2015-04-07 15:12:42 +1000

    colorize

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var fs          = require('fs');
 var gulp        = require('gulp'),
     mustache    = require('gulp-mustache-plus');
 
+var es          = require('event-stream');
+
 var http        = require('http'),
     connect     = require('connect'),
     serveStatic = require('serve-static');
@@ -26,6 +28,29 @@ gulp.task('serve', function () {
   http.createServer(app).listen(8000);
 });
 
+gulp.task('colorize', function () {
+  var colors = [
+    { name: 'none', value: 'none' },
+    { name: 'red', value: 'hsl(0, 100%, 30%)' },
+    { name: 'yellow', value: 'hsl(42, 100%, 54%)' },
+    { name: 'blue', value: 'hsl(206, 68%, 59%)' },
+    { name: 'blue-mid', value: 'hsl(206, 54%, 39%)' },
+    { name: 'green', value: 'hsl(79, 100%, 42%)' },
+    { name: 'green-mid', value: 'hsl(79, 100%, 38%)' },
+    { name: 'green-dark', value: 'hsl(79, 100%, 24%)' },
+    { name: 'grey', value: 'hsl(0, 0%, 30%)' },
+    { name: 'grey-light', value: 'hsl(0, 0%, 69%)' },
+    { name: 'navy', value: 'hsl(201, 100%, 14%)' }
+  ];
+  var streams = [];
 
+  colors.forEach(function (color) {
+    var colorStream = gulp.src('./patterns/*.svg')
+      .pipe(mustache({ color: color.value }, { extension: '.svg' }))
+      .pipe(gulp.dest('./patterns/' + color.name));
 
+    streams.push(colorStream);
+  });
 
+  return es.merge.apply(null, streams);
+});
